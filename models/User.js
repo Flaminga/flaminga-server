2ae8cdf95255c93624b94d3e4b415ba766831262
Logan Smyth <loganfsmyth@gmail.com>
2014-02-01 13:11:19 -0800

    Pull in starter from https://github.com/sahat/hackathon-starter.

---
+++ models/User.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+
+  facebook: { type: String, unique: true, sparse: true },
+  twitter: { type: String, unique: true, sparse: true },
+  google: { type: String, unique: true, sparse: true },
+  github: { type: String, unique: true, sparse: true },
+  tokens: Array,
+
+  profile: {
+    name: { type: String, default: '' },
+    gender: { type: String, default: '' },
+    location: { type: String, default: '' },
+    website: { type: String, default: '' },
+    picture: { type: String, default: '' }
+  }
+});
+
+/**
+ * Hash the password for security.
+ */
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  var SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', userSchema);
